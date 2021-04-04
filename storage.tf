476d5b902fa9afafa2830f3b060898753ec4f7ff
daniel <abdavl@almbrand.dk>
2021-04-04 19:38:41 +0200

    Add dependency to resource group

--- storage.tf
+++ storage.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "storage-account-rg" {
 
 resource "azurerm_storage_account" "backend_sa" {
   name                     = var.storage_account_name
-  resource_group_name      = var.sa_resource_group_name
+  resource_group_name      = azurerm_resource_group.storage_account_rg.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
